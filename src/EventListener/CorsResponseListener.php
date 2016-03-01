3790a7dbf1d8ec5b59c055cac86e3fa4cde1d4c3
Sanpi <sanpi@homecomputing.fr>
2016-03-01 20:54:36 +0100

    Add CORS headers

---
+++ src/EventListener/CorsResponseListener.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class CorsResponseListener
+{
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $headers = $event->getResponse()->headers;
+        $headers->set('Access-Control-Allow-Origin', '*');
+        $headers->set('Access-Control-Request-Method', '*');
+        $headers->set('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        $headers->set('Access-Control-Allow-Headers', '*');
+    }
+}
