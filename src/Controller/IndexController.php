e446ff8e1c93f3a7212298c7d3e2f43118945472
Sanpi <sanpi@homecomputing.fr>
2016-03-01 20:54:28 +0100

    Add /query

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 
 use \PommProject\Foundation\Pomm;
 use \Symfony\Component\HttpFoundation\JsonResponse;
+use \Symfony\Component\HttpFoundation\Request;
 
 class IndexController
 {
@@ -15,8 +16,62 @@ class IndexController
         $this->pomm = $pomm;
     }
 
-    public function indexAction(): JsonResponse
+    public function queryAction(Request $request): JsonResponse
     {
-        return new JsonResponse();
+        $queries = $request->query->get('q');
+
+        if ($queries === 'SHOW MEASUREMENTS LIMIT 1') {
+            $response = [
+                'results' => [],
+            ];
+        }
+        elseif ($queries !== '') {
+            $response = [
+                'results' => [
+                    [
+                        'series' => [],
+                    ],
+                ],
+            ];
+
+            foreach (explode("\n", $queries) as $query) {
+                if (empty($query)) {
+                    continue;
+                }
+
+                $query = $this->transformQuery($query);
+                $results = $this->pomm['default']->getQueryManager()
+                    ->query($query);
+
+                $serie = [
+                    'name' => md5($query),
+                    'columns' => [],
+                    'values' => [],
+                ];
+
+                if (count($results) > 0) {
+                    $serie['columns'] = array_keys($results->get(0));
+                }
+
+                foreach ($results as $result) {
+                    $serie['values'][] = array_values($result);
+                }
+
+                $response['results'][0]['series'][] = $serie;
+            }
+        }
+        else {
+            $response = 'OK';
+        }
+
+        return new JsonResponse($response);
+    }
+
+    private function transformQuery(string $query): string
+    {
+        $query = preg_replace('/time ((?:<|>) now\(\))/', 'created $1', $query);
+        $query = preg_replace('/time ((?:<|>) now\(\) -) (\d+\w)/', 'created $1 \'$2\'::interval', $query);
+        $query = preg_replace('/time ((?:<|>) \d+)s/', 'extract(epoch from created) $1', $query);
+        return $query;
     }
 }
