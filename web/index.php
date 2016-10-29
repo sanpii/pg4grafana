425315135f2a7344681a0a747a3e7e5399dc9ce6
Sanpi <sanpi@homecomputing.fr>
2016-10-29 17:38:08 +0200

    Use uniq entry point

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ use \Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$kernel = new \AppKernel('prod', false);
+$kernel = new \AppKernel();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
